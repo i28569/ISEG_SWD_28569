00a2cb06c548d7afc969acd85fc72d836f468230
i28569 <123770905+i28569@users.noreply.github.com>
2023-02-05 18:25:18 +0000

    login function created

--- LAB02/app.py
+++ LAB02/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,17 @@ def index():
 @app.route('/users/<username>')
 def show_user(username):
     return f'Hello {username} and welcome to my first web application!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    if username == 'test' and password == 'test':
+        return 'Login successful'
+    else:
+        return 'Invalid username or password'
+
 
 if __name__ == '__main__':
     app.run()
