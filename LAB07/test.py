4bf23eed110cbad6fbc371b94a0663018d0ba057
i28569 <i28569@aln.iseg.ulisboa.pt>
2023-02-25 21:53:37 +0000

    slightly change to test.py

--- LAB07/test.py
+++ LAB07/test.py
@@ -11,7 +11,7 @@ class FlaskTestCase(unittest.TestCase):
     def test_hello(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello World!')
+        self.assertEqual(response.data, b'Hello World 2!')
         
 if __name__ == '__main__':
      unittest.main()
